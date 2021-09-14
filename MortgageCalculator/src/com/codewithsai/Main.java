f73eda8aac7dec6769219b73e8cdc855b44cec60
Saikumar Karingula <saikumarkaringula9@gmail.com>
2021-09-14 12:56:21 -0400

    Added validations

--- MortgageCalculator/src/com/codewithsai/Main.java
+++ MortgageCalculator/src/com/codewithsai/Main.java
@@ -9,16 +9,35 @@ public class Main {
 
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
+
+        int principal = 0;
+        float annualInterest = 0.0F;
+        int period = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Principal : ");
-        long principal = scanner.nextLong();
+        System.out.println("Principle : ");
+        while(true) {
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter Principal value between 1000 and 1,000,000: ");
+        }
 
-        System.out.println("Annual Intrest Rate : ");
-        float annualInterest  = scanner.nextFloat();
-
-        System.out.println(" Period (Years) :");
-        int period = scanner.nextInt();
+        System.out.println("Annual Interest Rate : ");
+        while(true) {
+            annualInterest = scanner.nextFloat();
+            if(annualInterest > 0 && annualInterest <= 30 )
+                break;
+            System.out.println("Enter annual interest rate between 0 and 30 : ");
+        }
+        System.out.println("Period (Years) :");
+        while (true) {
+            period = scanner.nextInt();
+            if (period > 0 && period <= 30 )
+                break;
+            System.out.println(" Enter Period (Years) between 0 and 30:");
+        }
         int totalMonths = period* MONTHS_IN_YEAR;
         double monthlyInterest = (annualInterest/PERCENT)/MONTHS_IN_YEAR;
 
