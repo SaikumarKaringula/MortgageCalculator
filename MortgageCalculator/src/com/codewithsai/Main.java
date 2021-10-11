9de5e759d76c0f558871f8ec2bd729e664322cf4
Saikumar Karingula <saikumarkaringula9@gmail.com>
2021-10-11 12:24:07 -0400

    Code Refactoring

--- MortgageCalculator/src/com/codewithsai/Main.java
+++ MortgageCalculator/src/com/codewithsai/Main.java
@@ -9,18 +9,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int principal = ( int) ( readNumber("Enter Principle value", 1000, 1_000_000));
-        float annualInterest = ( float ) ( readNumber("Enter annualInterest value", 1, 30));
-        short period = ( short ) ( readNumber("Enter period value", 1, 30));;
+        int principal = ( int) ( readNumber("Enter Principle value between 1000 and 1_000_000", 1000, 1_000_000));
+
+        //Intrest Per Year need to convert to Monthly intrest Rate
+        float annualInterest = ( float ) ( readNumber("Enter annualInterest value between 1 and 30", 1, 30));
+
+        //In years
+        short period = ( short ) ( readNumber("Enter period value between 1 and 30", 1, 30));;
 
 
         Double mortgage = calculateMortgage(principal, annualInterest, period);
         String formattedMortage = NumberFormat.getCurrencyInstance().format(mortgage);
 
         System.out.println(" Mortgage ");
-        System.out.println("----------------");
+        System.out.println("-------------------");
 
         System.out.println("Your monthly payment is " +formattedMortage);
+
+        System.out.println("-------------------");
 
         calculateRemainingAmount(principal, annualInterest, period );
 
@@ -41,7 +47,7 @@ public class Main {
     public static double calculateMortgage(int principal, float annaualIntrestRate, short period){
 
         double monthlyInterest = ( annaualIntrestRate/PERCENT)/MONTHS_IN_YEAR;
-        int noOfPayments = period* MONTHS_IN_YEAR;
+        int noOfPayments = period * MONTHS_IN_YEAR;
 
         double mortgage = (principal)
                 * ( monthlyInterest * Math.pow(1+monthlyInterest, noOfPayments))
@@ -54,7 +60,7 @@ public class Main {
         double monthlyInterest = ( annaualIntrestRate/PERCENT)/MONTHS_IN_YEAR;
         int noOfPayments = period * MONTHS_IN_YEAR;
 
-        int noOfPaymentsMade = 0;
+        int noOfPaymentsMade = 1;
 
         double balance = principal;
         while(balance > 0 ){
